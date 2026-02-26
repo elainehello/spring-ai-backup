d9cc49057607d65d756cecfc09bd544f5de1effc
elainehello <195331169+elainehello@users.noreply.github.com>
2026-02-26 15:09:01 +0100

    Initial commit

---
+++ src/main/java/com/elainehello/spring_ai_backup/SpringAiBackupApplication.java
@@ -0,0 +1,13 @@
+package com.elainehello.spring_ai_backup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiBackupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elainehello/spring_ai_backup/SpringAiBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elainehello.spring_ai_backup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiBackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
